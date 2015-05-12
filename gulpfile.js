7db0d4e080c5138db9d997093b02b450e0deb16d
Dominic McAdden <dominic.mcadden@gmail.com>
2015-05-12 10:10:13 +0100

    added unit testing in

--- gulpfile.js
+++ gulpfile.js
@@ -73,6 +73,29 @@ gulp.task('sass', function () {
 
 
 //----------------------------------------------------------------
+// unit tests (jasmine)
+//----------------------------------------------------------------
+var karma = require('gulp-karma');
+
+gulp.task('unit', function() {
+  // Be sure to return the stream
+  // NOTE: Using the fake './foobar' so as to run the files
+  // listed in karma.conf.js INSTEAD of what was passed to
+  // gulp.src !
+  return gulp.src('./foobar')
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'run'
+    }))
+    .on('error', function(err) {
+      // Make sure failed tests cause gulp to exit non-zero
+      console.log(err);
+      this.emit('end'); //instead of erroring the stream, end it
+    });
+});
+
+
+//----------------------------------------------------------------
 // watch
 //----------------------------------------------------------------
 gulp.task('watch', function () {
