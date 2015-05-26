ae411bb3819ecca17cdfd8ad0a177a54146ce19f
Dominic McAdden <dominic.mcadden@gmail.com>
2015-05-26 16:15:07 +0100

    fixed controller unit test + added tests to ensure it talks to the socket

--- front_end/src/js/select/selectRockPaperOrScissors.controller.spec.js
+++ front_end/src/js/select/selectRockPaperOrScissors.controller.spec.js
@@ -22,14 +22,36 @@ describe('should allow the selection of rock, paper and scissors', function() {
 		expect(sut.isScissors()).toBe(true);
 	});
 
-	it('should register with current game', function() {
+	it('should register with current game once a player index has been received', function() {
+		sut.initWithPlayerIndex(0);
 		expect(mockCurrentGame.registerPlayer).toHaveBeenCalledWith(sut);
+	});
+
+	it('should register with socket once a game index has been received', function() {
+		sut.initWithGameIndex(0);
+		expect(mockSocket.registerPlayer).toHaveBeenCalledWith(sut);
 	});
 
 	it('should pass on isWinner request to current game', function() {
 		sut.isWinner();
 		expect(mockCurrentGame.isWinner).toHaveBeenCalledWith(sut);
 	});
+
+	it('should pass on selection to choice to the socket when rock is selected', function() {
+		sut.selectRock();
+		expect(mockSocket.send).toHaveBeenCalledWith(sut.ROCK);
+	});
+
+	it('should pass on selection to choice to the socket when paper is selected', function() {
+		sut.selectPaper();
+		expect(mockSocket.send).toHaveBeenCalledWith(sut.PAPER);
+	});
+
+	it('should pass on selection to choice to the socket when rock is selected', function() {
+		sut.selectScissors();
+		expect(mockSocket.send).toHaveBeenCalledWith(sut.SCISSORS);
+	});
+
 	
 	// ----------------------------------
 	// setup
@@ -86,13 +108,17 @@ describe('should allow the selection of rock, paper and scissors', function() {
 
 	function setupMockSocket() {
 		mockSocket = {
-			regiserPlayer: function(player) {
+			registerPlayer: function(player) {
 				// do nothing
 			},
 			send: function(msg) {
 				// do nothing
 			}
 		}
+
+		// set up spies so we can verify that the methods were called	
+    spyOn(mockSocket, 'registerPlayer').and.callThrough();	
+    spyOn(mockSocket, 'send').and.callThrough();	
 	}
 
 });
