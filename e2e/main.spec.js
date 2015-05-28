d9d76c98ba875a6deefa00c710a7c3953eadcec0
Dominic McAdden <dominic.mcadden@gmail.com>
2015-05-28 17:18:23 +0100

    basic e2e working again now - including the game being played across 2 browser windows

--- e2e/main.spec.js
+++ e2e/main.spec.js
@@ -49,12 +49,58 @@ describe('mvp version of rock paper scissors', function() {
 			});
 		});
 
-		it('should show icons for both players', function() {
-			shouldSeeRockPaperAndScissorsButtons(browser2, "#player1");
-			shouldSeeRockPaperAndScissorsButtons(browser2, "#player2");
-		});
+		it('should show Player 1 as the winner if Player 1 selects "Rock" and Player 2 selects "Scissors"', function() {
+			var player1Status;
+			var player2Status;
+			browser.element(by.css('#player1 #rock')).click().then(function() {
+				browser2.element(by.css('#player2 #scissors')).click().then(function() {
+					browser.element(by.css('#player1 #status')).getAttribute('class').then(function(classes) {
+						player1Status = classes;
+						browser2.element(by.css('#player2 #status')).getAttribute('class').then(function(classes) {
+							player2Status = classes;
+							expect(player1Status).toMatch('winner');
+							expect(player2Status).not.toMatch('winner');
+						});
+					});
+				});
+			});
+		})
 
-	})
+		it('should show Player 2 as the winner if Player 2 selects "Rock" and Player 1 selects "Scissors"', function() {
+			var player1Status;
+			var player2Status;
+			browser.element(by.css('#player1 #scissors')).click().then(function() {
+				browser2.element(by.css('#player2 #rock')).click().then(function() {
+					browser.element(by.css('#player1 #status')).getAttribute('class').then(function(classes) {
+						player1Status = classes;
+						browser2.element(by.css('#player2 #status')).getAttribute('class').then(function(classes) {
+							player2Status = classes;
+							expect(player1Status).not.toMatch('winner');
+							expect(player2Status).toMatch('winner');
+						});
+					});
+				});
+			});
+		})
+
+		it('should show no-one as the winner if Player 1 selects "Rock" and Player 2 also selects "Rock"', function() {
+			var player1Status;
+			var player2Status;
+			browser.element(by.css('#player1 #rock')).click().then(function() {
+				browser2.element(by.css('#player2 #rock')).click().then(function() {
+					browser.element(by.css('#player1 #status')).getAttribute('class').then(function(classes) {
+						player1Status = classes;
+						browser2.element(by.css('#player2 #status')).getAttribute('class').then(function(classes) {
+							player2Status = classes;
+							expect(player1Status).not.toMatch('winner');
+							expect(player2Status).not.toMatch('winner');
+						});
+					});
+				});
+			});
+		})
+
+	});
 
 	function shouldSeeRockPaperAndScissorsButtons(browser, player) {	
 		expect(browser.element(by.css(player+' #rock')).isPresent()).toBe(true);
@@ -97,105 +143,6 @@ describe('mvp version of rock paper scissors', function() {
 			});
 		});
 	}
-
-//	beforeEach(function() {
-//		browser.get(url);
-//
-//		// get redirected url
-//		browser.getCurrentUrl().then(function(url) {
-//			console.log("--------------------");
-//			console.log("url = " + url);	
-//			console.log("--------------------");
-//			browser2 = browser.forkNewDriverInstance();
-//			browser2.get(url);
-//		});
-//
-//	});
-//
-//	it('should have 2 browsers', function() {
-//		console.log("/nbrowser");
-//		console.log(browser);
-//		console.log("/nbrowser2");
-//		console.log(browser2);
-//		expect(browser2.not.toBeNull());
-//		expect(browser.not.toBeNull());
-//
-//		element(browser2.by.css(player+' #rock')).click().then(function() {
-//			console.log("clicked");
-//		});
-//	});
-//	describe('should work for player 1', function() {
-//		choices("#player1");
-//		highlight("#player1");
-//	});
-//
-//	describe('should work for player 2', function() {
-//		choices("#player2");
-//		highlight("#player2");
-//	});
-//
-//	function choices(player) {	
-//		it('should see a choice of Rock, Paper and Scissors', function() {
-//			expect(element(by.css(player+' #rock')).isPresent()).toBe(true);
-//			expect(element(by.css(player+' #paper')).isPresent()).toBe(true);
-//			expect(element(by.css(player+' #scissors')).isPresent()).toBe(true);
-//		});
-//	}
-//
-//	function highlight(player) {	
-//		it('should highlight Rock when it is clicked and all others should be unhighlighted', function() {
-//			var rockClasses, paperClasses, scissorsClasses;
-//			element(by.css(player+' #rock')).click().then(function() {
-//				element(by.css(player+' #rock')).getAttribute('class').then(function(classes) {
-//					rockClasses = classes;
-//					element(by.css(player+' #paper')).getAttribute('class').then(function(classes) {
-//						paperClasses = classes;
-//						element(by.css(player+' #scissors')).getAttribute('class').then(function(classes) {
-//							scissorsClasses = classes;
-//
-//							expect(rockClasses).toMatch('user_selected');
-//							expect(paperClasses).not.toMatch('user_selected');
-//							expect(scissorsClasses).not.toMatch('user_selected');
-//						});
-//					});
-//				});
-//			});
-//		});
-//	}
-//
-//	it('should show Player 1 as the winner if Player 1 selects "Rock" and Player 2 selects "Scissors"', function() {
-//		var player1Status;
-//		element(by.css('#player1 #rock')).click().then(function() {
-//			element(by.css('#player2 #scissors')).click().then(function() {
-//				element(by.css('#player1 #status')).getAttribute('class').then(function(classes) {
-//					player1Status = classes;
-//					expect(player1Status).toMatch('winner');
-//				});
-//			});
-//		});
-//	})
-//
-//	it('should not show Player 1 as the winner if Player 1 selects "Rock" and Player 2 hasn\'t selected yet', function() {
-//		var player1Status;
-//		element(by.css('#player1 #rock')).click().then(function() {
-//			element(by.css('#player1 #status')).getAttribute('class').then(function(classes) {
-//				player1Status = classes;
-//				expect(player1Status).not.toMatch('winner');
-//			});
-//		});
-//	})
-//
-//	it('should show Player 1 as the winner if Player 1 selects "Rock" and Player 2 also selects "Rock"', function() {
-//		var player1Status;
-//		element(by.css('#player1 #rock')).click().then(function() {
-//			element(by.css('#player2 #rock')).click().then(function() {
-//				element(by.css('#player1 #status')).getAttribute('class').then(function(classes) {
-//					player1Status = classes;
-//					expect(player1Status).not.toMatch('winner');
-//				});
-//			});
-//		});
-//	})
 
 })
 
