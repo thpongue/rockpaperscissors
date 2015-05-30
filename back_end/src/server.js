97b1b090df9ea213bfce238bd669350a1cd3b866
Dominic McAdden <dominic.mcadden@gmail.com>
2015-05-30 10:02:22 +0100

    minor refactor on the back end

--- back_end/src/server.js
+++ back_end/src/server.js
@@ -5,14 +5,6 @@ var uuid = require('uuid');
 var games = {};
 
 
-// notes
-// 1st user goes to site
-// gets redirected to game id
-// connects to our socket server passing this id
-// we put it into game.unique_id.players at position 0
-
-
-
 // -------------------------------------
 // server
 // -------------------------------------
@@ -51,26 +43,18 @@ var io = require('socket.io')(http);
 
 io.on('connection', function(socket){
 	var game_id = /game_id=(.*)/.exec(socket.request.headers.referer)[1];
-	console.log("connection attempt");
-	console.log("from socket id" + socket.id);
-	console.log("and game_id " + game_id);
-	console.log(socket.id);
+	console.log("connection attempt from socket id " + socket.id + " and game id: " + game_id);
 	
 	if (games[game_id]) {
+		var game = games[game_id];
 		console.log("I know this url!");
-		if (!games[game_id].players[0]) {
-			
-			// message to just that socket
-			io.to(socket.id).emit('position update', 0);
-
-			games[game_id].players[0] = socket.id;
+		if (!game.players[0]) {
+			io.to(socket.id).emit('position update', 0); // message to just that socket
+			game.players[0] = socket.id;
 			console.log("adding to position 0");
-		} else if (!games[game_id].players[1]) {
-
-			// message to just that socket
-			io.to(socket.id).emit('position update', 1);
-
-			games[game_id].players[1] = socket.id;
+		} else if (!game.players[1]) {
+			io.to(socket.id).emit('position update', 1); // message to just that socket
+			game.players[1] = socket.id;
 			console.log("adding to position 1");
 		}
 	} else {
@@ -79,11 +63,8 @@ io.on('connection', function(socket){
 	
   socket.on('game update', function(msg){
 		var game_id = /game_id=(.*)/.exec(socket.request.headers.referer)[1];
-		console.log("game update");
-		console.log("from socket id" + socket.id);
-		console.log("and game_id " + game_id);
+		console.log("game update from socket id " + socket.id + " and game id: " + game_id);
 		var players = games[game_id].players;
-
 		for(var i=0; i<players.length; i++) {
 			if (players[i] == socket.id) {
 				io.emit('game update', {index: i, value: msg});
@@ -95,11 +76,7 @@ io.on('connection', function(socket){
 	
   socket.on('disconnect', function(msg){
 		var game_id = /game_id=(.*)/.exec(socket.request.headers.referer)[1];
-		console.log("disconnected");
-		console.log("from socket id" + socket.id);
-		console.log("and game_id " + game_id);
-		console.log(socket.id);
-
+		console.log("disconnect from socket id " + socket.id + " and game id: " + game_id);
 		if (games[game_id]) {
 			if (games[game_id].players[0] == socket.id) {
 				games[game_id].players[0] = null;
