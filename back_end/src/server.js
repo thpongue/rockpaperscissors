32275257723f765564c8d048880540c53d7949cb
Dominic McAdden <dominic.mcadden@gmail.com>
2015-06-02 12:09:07 +0100

    back end refactoring - thank god / kent beck for my e2e tests

--- back_end/src/server.js
+++ back_end/src/server.js
@@ -11,13 +11,13 @@ var games = {};
 
 // redirect default request to a have a new game id
 app.get('/', function(request, response, next) {
-	var game_id = request.query.game_id;
-	if (!game_id) {
-		game_id = uuid.v4();
-		games[game_id] = {
+	var gameId = request.query.gameId;
+	if (!gameId) {
+		gameId = uuid.v4();
+		games[gameId] = {
 			players: []
 		};
-		response.redirect('?game_id='+game_id);
+		response.redirect('?gameId='+gameId);
 	} else {
 		next();
 	}
@@ -43,10 +43,10 @@ var io = require('socket.io')(http);
 var maxNumberOfPlayers = 2;
 
 io.on('connection', function(socket){
-	var game_id = /game_id=(.*)/.exec(socket.request.headers.referer)[1];
-	var game = games[game_id];
+	var gameId = getGameId(socket);
+	var game = games[gameId];
 	
-	console.log("connection attempt from socket id " + socket.id + " and game id: " + game_id);
+	logInfo("connection attempt ", socket.id, gameId);
 
 	if (game) {
 		console.log("This is a valid url");
@@ -61,15 +61,16 @@ io.on('connection', function(socket){
 				io.to(players[j]).emit('another player connect'); // message to just that socket
 			}
 		}
-
 	} else {
 		console.log("unrecognised url");
 	}
 	
   socket.on('game update', function(msg){
-		var game_id = /game_id=(.*)/.exec(socket.request.headers.referer)[1];
-		console.log("game update from socket id " + socket.id + " and game id: " + game_id);
-		var game = games[game_id];
+		var gameId = getGameId(socket);
+		var game = games[gameId];
+
+		logInfo("game update", socket.id, gameId);
+
 		if (game) {
 			var players = game.players;
 			var index = players.indexOf(socket.id);
@@ -86,19 +87,23 @@ io.on('connection', function(socket){
   });
 	
   socket.on('disconnect', function(msg){
-		var game_id = /game_id=(.*)/.exec(socket.request.headers.referer)[1];
-		console.log("disconnect from socket id " + socket.id + " and game id: " + game_id);
-		if (games[game_id]) {
-			if (games[game_id].players[0] == socket.id) {
-				games[game_id].players[0] = null;
-				console.log("remove from position 0");
-			} else if (games[game_id].players[1] == socket.id) {
-				games[game_id].players[1] = null;
-				console.log("remove from position 1");
-			}
+		var gameId = getGameId(socket);
+		var game = games[gameId];
+
+		logInfo("disconnect", socket.id, gameId);
+
+		if (game) {
+			var players = game.players;
+			var index = players.indexOf(socket.id);
+			players[index]=null;
+			console.log("remove from position " + index);
 		}
   });
 });
+
+function getGameId(socket) {
+	return /gameId=(.*)/.exec(socket.request.headers.referer)[1];
+}
 
 function getIndexForNewPlayer(players) {
 	for (var i=0; i<maxNumberOfPlayers; i++) {
@@ -107,6 +112,10 @@ function getIndexForNewPlayer(players) {
 		}
 	}
 	return null;
+}
+
+function logInfo(prefix, socketId, gameId) {
+		console.log(prefix + " from socket id " + socketId + " and game id: " + gameId);
 }
 
 http.listen(3000, function(){
